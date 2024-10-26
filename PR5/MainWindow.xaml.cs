8f5e645d41893d03ff148746b82fe99ce84ca776
исп-34 <-34@LAB24-07>
2024-10-26 10:38:45 +0300

    Добавьте файлы проекта.

---
+++ PR5/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TriardClass;
+
+namespace PR5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalculater_Click(object sender, RoutedEventArgs e)
+        {
+            Triard triard = new Triard();
+            bool boolOnePositiveNumber = int.TryParse(tbOnePositiveNumber.Text, out int oneNumber);
+            bool boolTwoPositiveNumber = int.TryParse(tbTwoPositiveNumber.Text, out int twoNumber);
+            bool boolThreePositiveNumber = int.TryParse(tbThreePositiveNumber.Text, out int threeNumber);
+            if(boolOnePositiveNumber == true && boolTwoPositiveNumber == true && boolThreePositiveNumber == true)
+            {
+                int onePositiveNumber = Convert.ToInt32(oneNumber);
+                int twoPositiveNumber = Convert.ToInt32(twoNumber);
+                int threePositivenumber = Convert.ToInt32(threeNumber);
+                triard.SetParams(onePositiveNumber,twoPositiveNumber,threePositivenumber);
+                int Sum = triard.SumNumber();
+                tbCalculater.Text = Convert.ToString(Sum);
+            }
+            else if (boolOnePositiveNumber == true && boolTwoPositiveNumber == true)
+            {
+                int onePositiveNumber = Convert.ToInt32(oneNumber);
+                int twoPositiveNumber = Convert.ToInt32(twoNumber);
+                triard.SetParams(onePositiveNumber, twoPositiveNumber);
+                int Sum = triard.SumNumber();
+                tbCalculater.Text = Convert.ToString(Sum);
+            }
+            else if (boolOnePositiveNumber == true)
+            {
+                int onePositiveNumber = Convert.ToInt32(oneNumber);
+                triard.SetParams(onePositiveNumber);
+                int Sum = triard.SumNumber();
+                tbCalculater.Text = Convert.ToString(Sum);
+            }
+            else MessageBox.Show("Введите числа!!!");
+        }
+        private void btInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Сделал:Ермаков Павел\nГруппа:ИСП-34\nЗадание:Создать класс Triad (тройка положительных чисел). Создать необходимые методы и свойства." +
+                " Определить метод вычисления суммы чисел. Создать перегруженные методы SetParams, для установки параметров объекта.");
+        }
+
+        private void btExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ PR5/TriardClass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriardClass
+{
+    class Triard
+    {
+        public int onePositiveNumber;
+        public int twoPositiveNumber;
+        public int threePositiveNumber;
+        public int OnePositiveNumber
+        { 
+            get 
+            { 
+                return onePositiveNumber;
+            }
+            set
+            {
+                if (value > 0) onePositiveNumber = value;
+            }
+        }
+        public int TwoPositiveNumber
+        {
+            get
+            {
+                return twoPositiveNumber;
+            }
+            set
+            {
+                if (value > 0) twoPositiveNumber = value;
+            }
+        }
+        public int ThreePositiveNumber
+        {
+            get
+            {
+                return threePositiveNumber;
+            }
+            set
+            {
+                if (value > 0) threePositiveNumber = value;
+            }
+        }
+        public int SumNumber()
+        {
+            return OnePositiveNumber + TwoPositiveNumber + ThreePositiveNumber;
+        }
+        public void SetParams(int oneSetParams, int twoSetParams, int threeSetParams)
+        {
+            OnePositiveNumber = oneSetParams;
+            TwoPositiveNumber = twoSetParams;
+            ThreePositiveNumber = threeSetParams;
+        }
+        public void SetParams(int oneSetParams, int twoSetParams)
+        {
+            OnePositiveNumber = oneSetParams;
+            TwoPositiveNumber = twoSetParams;
+            ThreePositiveNumber = oneSetParams + twoSetParams;
+        }
+        public void SetParams(int oneSetParams)
+        {
+            OnePositiveNumber = oneSetParams;
+            TwoPositiveNumber = oneSetParams;
+            ThreePositiveNumber = oneSetParams;
+        }
+    }
+}
